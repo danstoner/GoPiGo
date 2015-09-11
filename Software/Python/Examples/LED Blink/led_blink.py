d36c760cbc524fceb76a124e7b5b3c5d985af056
Dan Stoner <danstoner@gmail.com>
2015-09-11 19:43:20 -0400

    add clean shutdown to turn off LEDs on keyboard interrupt

--- Software/Python/Examples/LED Blink/led_blink.py
+++ Software/Python/Examples/LED Blink/led_blink.py
@@ -5,13 +5,21 @@ from gopigo import *
 import sys
 import time
 
-while True:
-	print "ON"
-	led_on(LED_L)
-	led_on(LED_R)
-	time.sleep(.5)
-	
-	print "OFF"
-	led_off(LED_L)
-	led_off(LED_R)
-	time.sleep(.5)
+# We wrap the loop with try/except in order to make sure that we turn off the
+# leds when the script is interrupted with ctrl-c
+try:
+        while True:
+                print "ON"
+                led_on(LED_L)
+                led_on(LED_R)
+                time.sleep(.5)
+
+                print "OFF"
+                led_off(LED_L)
+                led_off(LED_R)
+                time.sleep(.5)
+except KeyboardInterrupt:
+        led_off(LED_L)
+        led_off(LED_R)
+        raise SystemExit
+
